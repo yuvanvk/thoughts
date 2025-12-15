bcfe6ccac0d59271ef00dd39a7dfa3b8771bed95
yuvanvk <abhivigneshofficial@gmail.com>
2025-12-15 19:01:01 +0530

    fix: hero section

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
       <Navbar />
       <div className="w-full h-full max-w-6xl mx-auto py-40">
         <div className="flex flex-col items-center space-y-5">
-          <button className="px-8 py-3 bg-neutral-700 uppercase font-medium font-mono text-sm">
+          <button className="px-8 py-3 bg-neutral-700 text-neutral-100 uppercase font-medium font-mono text-sm">
             A minimal Blogging platform
           </button>
           <h1 className="font-sans text-7xl font-medium text-center max-w-xl">
@@ -17,7 +17,15 @@ export default function Page() {
             A minimal blogging space designed for those who value expression
             over perfection. Share ideas that matter.
           </p>
+          <div className="flex items-center gap-x-8">
+            <button className="px-8 py-3 bg-white text-black uppercase font-medium font-mono text-sm cursor-pointer">
+              Start writing
+            </button>
 
+            <button className="px-8 py-3 bg-black text-neutral-400 uppercase font-medium font-mono text-sm border cursor-pointer">
+              Read blogs
+            </button>
+          </div>
         </div>
       </div>
     </div>
