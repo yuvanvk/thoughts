6c04dcbb454abc5b1ad72b754f73176cd765837d
yuvanvk <abhivigneshofficial@gmail.com>
2025-12-27 14:19:32 +0530

    fix: formatDate db date-string

--- apps/web/components/blog/detailed-blog.tsx
+++ apps/web/components/blog/detailed-blog.tsx
@@ -3,9 +3,10 @@
 import { useTRPC } from "@/lib/trpc/trpc";
 import { useQuery } from "@tanstack/react-query";
 import { Button } from "@workspace/ui/components/button";
-import { ArrowLeft, Loader } from "lucide-react";
+import { ArrowLeft } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import { Skeleton } from "@workspace/ui/components/skeleton";
+import parse from "html-react-parser";
 import Image from "next/image";
 import { toast } from "sonner";
 
@@ -23,11 +24,31 @@ export const DetailedBlog = () => {
   if (error) {
     toast.error(error.message);
     router.push("/home");
-    return
+    return;
+  }
+
+  function estimatedTime(content: string) {
+    if (!content) return;
+
+    const text = content.replace(/<[^>]*>/g, "");
+    const words = text.trim().split(/\s+/).length;
+    const minutes = Math.ceil(words / 200);
+
+    return minutes;
+  }
+
+  function formatDate(date: string) {
+    if (!date) return "";
+    const dateObj =  new Date(date);
+    return new Intl.DateTimeFormat("en-US", {
+      month: "short",
+      day: "numeric",
+      year: "numeric",
+    }).format(dateObj);
   }
 
   return (
-    <div className="flex flex-col max-w-6xl mx-auto py-28 space-y-6">
+    <div className="flex flex-col max-w-6xl mx-auto py-28 space-y-6 w-full">
       <div className="w-fit">
         <Button
           variant={"secondary"}
@@ -54,7 +75,7 @@ export const DetailedBlog = () => {
         <div className="flex flex-col max-w-3/4">
           <div className="text-5xl tracking-tight font-medium">
             {isLoading ? (
-              <Skeleton className="w-full md:w-28 lg:w-xl h-12" />
+              <Skeleton className="w-full md:w-28 lg:w-xl xl:w-4xl h-12" />
             ) : (
               `${data?.blog?.title}`
             )}
@@ -64,16 +85,23 @@ export const DetailedBlog = () => {
               {isLoading ? (
                 <Skeleton className="w-64 h-5" />
               ) : (
-                `${data?.blog?.createdAt}`
+                formatDate(data?.blog?.createdAt!)
               )}
             </div>
             <span className="font-mono text-sm text-neutral-400">
               {isLoading ? (
                 <Skeleton className="w-20 h-5" />
               ) : (
-                `· ${8} min read`
+                `· ${estimatedTime(data?.blog.description!)} min read`
               )}
             </span>
+          </div>
+          <div className="text-xl tracking-tight font-medium font-mono my-10">
+            {isLoading ? (
+              <Skeleton className="w-full xl:w-6xl h-32 flex-1" />
+            ) : (
+              parse(`${data?.blog.description}`)
+            )}
           </div>
         </div>
       </div>
