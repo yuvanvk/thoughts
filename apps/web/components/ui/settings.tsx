3e13a2003d34d6ca58bbee6b76f566566fe9a219
yuvanvk <abhivigneshofficial@gmail.com>
2026-01-15 11:28:19 +0530

    fix: settings-page design and layout

--- apps/web/components/ui/settings.tsx
+++ apps/web/components/ui/settings.tsx
@@ -1,81 +1,126 @@
 "use client";
 
+import { Button } from "@workspace/ui/components/button";
 import { Input } from "@workspace/ui/components/input";
 import { Label } from "@workspace/ui/components/label";
+import { cn } from "@workspace/ui/lib/utils";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export const Settings = () => {
   const router = useRouter();
 
+  const [settings, setSettings] = useState({
+    firstName: "",
+    lastName: "",
+    password: ""
+  });
+
   return (
-    <div className="max-w-6xl mx-auto border-x h-screen">
-      <div className="px-16 py-4 flex flex-col">
+    <div>
+      <div className="px-16 py-4 flex flex-col font-sans">
         <div className="space-y-3">
-          <div className="text-5xl font-medium font-sans">Settings</div>
-          <div className="text-sm text-neutral-400 font-normal font-mono">
-            Manage your account and preferences
+          <div>
+            <div className="text-2xl font-medium">Settings</div>
+            <div className="text-xs text-neutral-400 font-normal">
+              Manage your account and preferences
+            </div>
           </div>
           <div className="my-10 space-y-10">
-            <div className="space-y-3">
+            <div className="space-y-2">
               <Label
                 htmlFor="firstname"
-                className="uppercase font-light font-mono text-[#A0A0A0]"
+                className={cn("font-medium  text-white")}
               >
-                First Name
+                First name
               </Label>
               <Input
+                onChange={(e) => {
+                  setSettings({
+                    ...settings,
+                    firstName: e.target.value
+                  })
+                }}
                 placeholder="Yuvan"
-                className="font-sans uppercase !bg-black !px-4 !py-6"
+                className={cn(
+                  "rounded-[10px] border",
+                  "dark:bg-neutral-800 dark:border-neutral-700"
+                )}
               />
             </div>
-            <div className="space-y-3">
+
+            <div className="space-y-2">
               <Label
                 htmlFor="lastname"
-                className="uppercase font-light font-mono text-[#A0A0A0]"
+                className={cn("font-medium  text-white")}
               >
                 Last name
               </Label>
               <Input
-              
+                onChange={(e) => setSettings({
+                  ...settings,
+                  lastName: e.target.value
+                })}
                 placeholder="Kappala"
-                className="font-sans uppercase !bg-black !px-4 !py-6"
+                className={cn(
+                  "rounded-[10px] border",
+                  "dark:bg-neutral-800 dark:border-neutral-700"
+                )}
               />
-              
             </div>
-            <div className="space-y-3">
-              <Label
-                htmlFor="email"
-                className="uppercase font-light font-mono text-[#A0A0A0]"
-              >
-                email
+            <div className="space-y-2">
+              <Label htmlFor="email" className={cn("font-medium  text-white")}>
+                Email
               </Label>
               <Input
                 disabled
                 value={"abhivigneshofficial@gmail.com"}
-                className="font-sans uppercase !bg-black !px-4 !py-6"
+                className={cn(
+                  "rounded-[10px] border",
+                  "dark:bg-neutral-800 dark:border-neutral-700"
+                )}
               />
             </div>
-            <div className="space-y-3">
+            <div className="space-y-2">
               <Label
-                htmlFor="email"
-                className="uppercase font-light font-mono text-[#A0A0A0]"
+                htmlFor="password"
+                className={cn("font-medium  text-white")}
               >
                 Password
               </Label>
               <Input
+                onChange={(e) => setSettings({
+                  ...settings,
+                  password: e.target.value
+                })}
                 placeholder="*****"
-                className="font-sans uppercase !bg-black !px-4 !py-6"
+                className={cn(
+                  "rounded-[10px] border",
+                  "dark:bg-neutral-800 dark:border-neutral-700"
+                )}
               />
-              <div className="text-right underline text-sm font-mono text-neutral-400 cursor-pointer">Change password</div>
+              <div className="text-right underline text-xs  text-neutral-200 cursor-pointer">
+                Change password
+              </div>
             </div>
 
-            <div className="flex items-center justify-between font-mono text-sm">
-                <button onClick={() => router.push("/home")} className="bg-[#121212] px-5 py-2 border cursor-pointer">
-                    Cancel
-                </button>
-                <button className="bg-white text-black px-5 py-2 border">
-                    Save
-                </button>
+            <div className="flex items-center justify-between text-sm">
+              <Button
+                onClick={() => router.push("/home")}
+                size={"sm"}
+                className={cn("rounded-[10px] shadow px-5 py-2 cursor-pointer")}
+              >
+                Back
+              </Button>
+              <Button
+                size={"sm"}
+                className={cn(
+                  "text-white rounded-[10px] shadow px-5 py-2 cursor-pointer",
+                  "bg-green-600 hover:bg-green-700"
+                )}
+              >
+                Save
+              </Button>
             </div>
           </div>
         </div>
