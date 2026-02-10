cb24e9bef7f242f369d696da4c4e7101e2f42b27
yuvanvk <abhivigneshofficial@gmail.com>
2026-02-10 18:30:46 +0530

    chore: add footer for landing page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,4 +1,5 @@
 import { AvatarSection } from "@/components/landing/avatar-section";
+import { Footer } from "@/components/landing/footer";
 import { Hero } from "@/components/landing/hero";
 import { Navbar } from "@/components/landing/navbar";
 import { Container } from "@/components/wrapper/container";
@@ -9,6 +10,7 @@ export default function Page() {
       <Navbar />
       <Hero /> 
       <AvatarSection />
+      <Footer />
    </Container>
   )
 }
---
+++ apps/web/components/landing/footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <div className="font-serif text-[18px] text-center max-w-md mx-auto fixed bottom-4 right-5 text-neutral-500">
+      Made with ❤️ by yvk
+    </div>
+  );
+};
